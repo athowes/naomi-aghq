56cd142c5c3c554452b62d2656b017ec1c336ec3
Adam Howes <adamhowes@outlook.com>
2021-09-21 15:36:23 +0100

    Changing simulation values to match Eaton (2019) #009

--- src/prev-anc-art_sim/script.R
+++ src/prev-anc-art_sim/script.R
@@ -7,27 +7,27 @@ set.seed(1)
 #' Simulate data
 
 #' Survey prevalence
-n <- 5
-m_prev <- rep(20, n)
-beta_prev <- -1
-tau_phi_prev <- 1
+n <- 30
+m_prev <- rep(250, n)
+beta_prev <- -2.4
+tau_phi_prev <- 4 #' sigma_phi_prev is 0.5
 eta_prev <- beta_prev + rnorm(n, 0, 1 / sqrt(tau_phi_prev))
 rho_prev <- plogis(eta_prev)
 y_prev <- rbinom(n, m_prev, rho_prev)
 
 #' ANC
-m_anc <- rep(50, n)
-beta_anc <- 0.5
-tau_b_anc <- 4
+m_anc <- rep(10^4, n)
+beta_anc <- -0.2
+tau_b_anc <- 100 #' tau_b_anc is 0.1
 b_anc <- beta_anc + rnorm(n, 0,  1 / sqrt(tau_b_anc))
 eta_anc <- eta_prev + b_anc
 rho_anc <- plogis(eta_anc)
 y_anc <- rbinom(n, m_anc, rho_anc)
 
 #' ART
-N_art <- rep(500, n)
-beta_art <- 0
-tau_phi_art <- 1
+N_art <- rep(10^5, n)
+beta_art <- 0.7
+tau_phi_art <- (1 / 0.35)^2 #' tau_phi_art is 0.1
 eta_art <- beta_art + rnorm(n, 0, 1 / sqrt(tau_phi_art))
 alpha_art <- plogis(eta_art)
 A_art <- rbinom(n, floor(N_art * rho_prev), alpha_art)
