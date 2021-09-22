0ee29a3799f7d882af05777bff42f4ca3e0ed07c
Adam Howes <adamhowes@outlook.com>
2021-09-22 19:34:22 +0100

    Plot based on parameter names #009

--- src/prev-anc-art_process-results/functions.R
+++ src/prev-anc-art_process-results/functions.R
@@ -1,12 +1,10 @@
 draw_boxplots <- function(results, methods) {
   map(results, "comparison_results") %>%
-    map(tibble::rownames_to_column, var = "param_id") %>%
     bind_rows(.id = "sim_id") %>%
     pivot_longer(cols = all_of(methods), names_to = "method", values_to = "value") %>%
-    mutate(across(c(param_id, sim_id), as.numeric)) %>%
     ggplot(aes(x = method, y = value, fill = method)) +
       geom_boxplot() +
-      facet_wrap(~param_id, ncol = 2, scales = "free") +
+      facet_wrap(parameter ~ type, scales = "free") +
       scale_fill_manual(values = cbpalette) +
       labs(x = "", y = "Posterior mean", fill = "Inference method")
 }
