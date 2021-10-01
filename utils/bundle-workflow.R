52b4b2089869851b2b4438789f5f2f9a31bc72e9
Adam Howes <ath19@dide.local>
2021-10-01 18:02:11 +0100

    Still trying #008

--- utils/bundle-workflow.R
+++ utils/bundle-workflow.R
@@ -18,14 +18,20 @@ spud <- spud::sharepoint$new("https://imperiallondon.sharepoint.com/")
 folder <- spud$folder("HIVInferenceGroup-WP", "Shared Documents/orderly/naomi-inf/bundle-input", verify = TRUE)
 folder$upload(path = bundle$path)
 
+recent_bundle <- folder$list() %>%
+  filter(created == max(created))
+
 #' B1.
-folder$download(path = "20210930-163548-5576c904.zip", dest = "bundle-input/20210930-163548-5576c904.zip")
+folder$download(
+  path = recent_bundle$name,
+  dest = paste0("bundle-input/", recent_bundle$name)
+)
 
 #' B2.
 orderly_packages <- yaml::read_yaml(file.path("src/prev-anc-art_model0-N/orderly.yml"))$packages
 packages <- list(loaded = c("orderly", orderly_packages))
 
-my_config <- didehpc::didehpc_config(
+config <- didehpc::didehpc_config(
   workdir = path_bundles,
   credentials = "ath19",
   cluster = "fi--didemrchnb"
@@ -33,10 +39,17 @@ my_config <- didehpc::didehpc_config(
   # "fi--didemrchnb"
 )
 
+src <- conan::conan_sources("awstringer1/aghq")
+
 ctx <- context::context_save(
   "context",
   packages = packages,
-  package_sources = conan::conan_sources("awstringer1/aghq")
+  package_sources = src
 )
 
-obj <- didehpc::queue_didehpc(ctx, config = my_config)
+obj <- didehpc::queue_didehpc(ctx, config = config)
+
+t <- obj$enqueue(orderly::orderly_bundle_run(recent_bundle$name, "bundle-output"))
+
+t$status()
+t$result()
