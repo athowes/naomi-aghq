7bf454b383c0793553c9535fb29ebdff5957edd7
Adam Howes <adamhowes@outlook.com>
2022-02-01 22:30:03 +0000

    Missed this one, removing -N

--- src/prev-anc-art_sim/script.R
+++ src/prev-anc-art_sim/script.R
@@ -1,6 +1,6 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("prev-anc-art_sim-N")
-# setwd("src/prev-anc-art_sim-N")
+# orderly::orderly_develop_start("prev-anc-art_sim")
+# setwd("src/prev-anc-art_sim")
 
 N_sim <- 1000
 
