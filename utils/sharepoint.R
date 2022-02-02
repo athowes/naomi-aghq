3bb3f136ea37307af3fa9424fd857a7fccbbed5d
Adam Howes <adamhowes@outlook.com>
2022-02-02 17:08:56 +0000

    Don't need this sharepoint details

--- utils/sharepoint.R
+++
@@ -1,9 +0,0 @@
-#' Setting details required by spud to reach the sharepoint as environmental variables
-pass <- "ENTER_PASS_HERE"
-
-Sys.setenv(
-  "SHAREPOINT_USERNAME" = "ath19@ic.ac.uk",
-  "SHAREPOINT_PASS" = pass,
-  "SHAREPOINT_URL" = "https://imperiallondon.sharepoint.com/",
-  "SHAREPOINT_SITE" = "HIVInferenceGroup-WP"
-)
