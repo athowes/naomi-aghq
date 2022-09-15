5095622cb6cde0d7075b076c1e957855c20c0e65
athowes <adamhowes@outlook.com>
2022-09-15 12:01:54 -0400

    For loop over indicies of latent field doing Gaussian approximations

--- src/dev_sinla/model1index.cpp
+++ src/dev_sinla/model1index.cpp
@@ -6,24 +6,22 @@ template <class Type>
 Type objective_function<Type>::operator()()
 {
   // Data block
-  DATA_INTEGER(n);      // Number of regions
-  DATA_VECTOR(y_prev);  // Vector of survey responses
-  DATA_VECTOR(m_prev);  // Vector of survey sample sizes
-  DATA_INTEGER(i);      // Index of latent field element marginal
+  DATA_INTEGER(n);             // Number of regions
+  DATA_VECTOR(y_prev_i);       // Survey response for index i
+  DATA_VECTOR(y_prev_minus_i); // Survey response for index -i
+  DATA_VECTOR(m_prev_i);       // Survey sample size for index i
+  DATA_VECTOR(m_prev_minus_i); // Survey sample size for index -i
 
   // Parameter block
-  PARAMETER(beta_prev);              // Survey intercept
-  PARAMETER(phi_prev_i);             // Survey spatial effect for index i
-  PARAMETER_VECTOR(phi_prev_minus_i) // Survey spatial effect for index -i
-  PARAMETER(log_sigma_phi_prev);     // Survey log standard deviation of spatial effects
+  PARAMETER(beta_prev);               // Survey intercept
+  PARAMETER(phi_prev_i);              // Survey spatial effect for index i
+  PARAMETER_VECTOR(phi_prev_minus_i); // Survey spatial effect for index -i
+  PARAMETER(log_sigma_phi_prev);      // Survey log standard deviation of spatial effects
 
   // Transformed parameters block
   Type sigma_phi_prev = exp(log_sigma_phi_prev);
-  // Create phi_prev = (phi_prev_i, phi_prev_minus_i)
-  vector<Type> eta_prev(beta_prev + sigma_phi_prev * phi_prev);
-  // Assuming we can pick out elements of a vector in TMB then:
-  // Create y_prev_i here
-  // Create y_prev_minus_i here
+  Type eta_prev_i = beta_prev + sigma_phi_prev * phi_prev_i;
+  vector<Type> eta_prev_minus_i(beta_prev + sigma_phi_prev * phi_prev_minus_i);
 
   // Initialise negative log-likelihood
   Type nll(0.0);
@@ -31,13 +29,14 @@ Type objective_function<Type>::operator()()
   // Priors
   nll -= dnorm(sigma_phi_prev, Type(0), Type(2.5), true) + log_sigma_phi_prev;
   nll -= dnorm(beta_prev, Type(-2), Type(1), true);
-  nll -= dnorm(phi_prev_i, Type(0), Type(1), true).sum();
+  nll -= dnorm(phi_prev_i, Type(0), Type(1), true);
+  nll -= dnorm(phi_prev_minus_i, Type(0), Type(1), true).sum();
 
   // Likelihood
-  nll -= dbinom_robust(y_prev, m_prev, eta_prev, true).sum();
+  nll -= dbinom_robust(y_prev_i, m_prev_i, eta_prev_i, true).sum();
+  nll -= dbinom_robust(y_prev_minus_i, m_prev_minus_i, eta_prev_minus_i, true).sum();
 
   // Generated quantities block
-  vector<Type> rho_prev(invlogit(eta_prev));    // Posterior prevalence estimates
   Type tau_phi_prev = 1/pow(sigma_phi_prev, 2); // Precision of spatial effects
 
   return(nll);
--- src/dev_sinla/script.R
+++ src/dev_sinla/script.R
@@ -5,48 +5,66 @@
 sim_data <- readRDS("depends/sim_data.rds")
 data <- sim_data[[1]] #' Only need once instance of the simulated data
 
-#' TMB preparation
-compile("model1.cpp")
-dyn.load(dynlib("model1"))
+compile("model1index.cpp")
+dyn.load(dynlib("model1index"))
 
 dat <- list(n = data$n, y_prev = data$y_prev, m_prev = data$m_prev)
 
-#' TMB
+prepare_index <- function(dat, i) {
+  dat[["y_prev_i"]] <- dat$y_prev[i]
+  dat[["y_prev_minus_i"]] <- dat$y_prev[-i]
+  dat[["m_prev_i"]] <- dat$m_prev[i]
+  dat[["m_prev_minus_i"]] <- dat$m_prev[-i]
+  dat[c("n", "y_prev_i", "y_prev_minus_i", "m_prev_i", "m_prev_minus_i")]
+}
+
+#' Starting parameters for TMB are the same for every index of the loop
 param <- list(
   beta_prev = 0,
-  phi_prev = rep(0, data$n),
+  phi_prev_i = 0,
+  phi_prev_minus_i = rep(0, data$n - 1),
   log_sigma_phi_prev = 0
 )
 
-#' random are integrated out with a Laplace approximation
-obj <- MakeADFun(
-  data = dat,
-  parameters = param,
-  random = "phi_prev", # Random effects to be integrated out
-  DLL = "model1"
-)
+#' Loop over doing the Gaussian approximation for every index i
+mean <- list()
+prec <- list()
 
-its <- 1000
-opt <- nlminb(
-  start = obj$par,
-  objective = obj$fn,
-  gradient = obj$gr,
-  control = list(iter.max = its, trace = 0)
-)
+for(i in 1:dat$n) {
+  dat_i <- prepare_index(dat, i)
 
-sd_out <- sdreport(
-  obj,
-  par.fixed = opt$par,
-  getJointPrecision = TRUE
-)
+  #' random are integrated out with a Laplace approximation
+  obj <- MakeADFun(
+    data = dat1,
+    parameters = param,
+    random = "phi_prev_minus_i", # Random effects to be integrated out
+    DLL = "model1index"
+  )
 
-#' aghq
-quad <- aghq::marginal_laplace_tmb(
-  obj,
-  k = 3,
-  startingvalue = c(param$beta_prev, param$log_sigma_phi_prev)
-)
+  its <- 1000
 
-#' Comparison
-tmb <- tmb_summary(sd_out)
-aghq <- aghq_summary(quad)
+  opt <- nlminb(
+    start = obj$par,
+    objective = obj$fn,
+    gradient = obj$gr,
+    control = list(iter.max = its, trace = 0)
+  )
+
+  sd_out <- sdreport(
+    obj,
+    par.fixed = opt$par,
+    getJointPrecision = TRUE
+  )
+
+  mean[[i]] <- 1
+  prec[[i]] <- sd_out$jointPrecision
+}
+
+mean
+prec
+
+pdf("prec.pdf", h = 5, w = 6.25)
+
+lapply(prec, image)
+
+dev.off()
