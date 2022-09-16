9495244c40d5377303f0e4d13a1e642693db21c8
athowes <adamhowes@outlook.com>
2022-09-16 16:08:11 -0400

    Start working on Naomi vignette example

---
+++ src/example_naomi/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("example_naomi")
+# setwd("src/example_naomi")
+
+rmarkdown::render("naomi.Rmd")
