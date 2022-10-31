1ab0d69f207c67f0278395313d4dbd99468811fa
athowes <adamhowes@outlook.com>
2022-10-31 12:09:38 -0400

    Set-up structure for posterior-comparison notebook

---
+++ src/explore_posterior-comparison/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("explore_posterior-comparison")
+# setwd("src/explore_posterior-comparison")
+
+rmarkdown::render("posterior-comparison.Rmd")
