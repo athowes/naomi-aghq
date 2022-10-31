fc1fb2c12a7734d397b7c8f2b5b5ecab742801a4
athowes <adamhowes@outlook.com>
2022-10-31 10:54:09 -0400

    Rename process-results to results

--- src/prev-anc-art_results/script.R
+++ src/prev-anc-art_results/script.R
@@ -1,5 +1,5 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("prev-anc-art_process-results")
-# setwd("src/prev-anc-art_process-results")
+# orderly::orderly_develop_start("prev-anc-art_results")
+# setwd("src/prev-anc-art_results")
 
 rmarkdown::render("prev-anc-art.Rmd")
