9f4fe239eab2d0cc9a9f3a470b04458d34f4edb9
athowes <adamhowes@outlook.com>
2022-11-09 14:32:30 -0500

    Structure things so that dev_sinla can run without running the experiments section

--- make/_make.R
+++ make/_make.R
@@ -4,7 +4,14 @@ run_commit_push("example_inla-replication")
 run_commit_push("example_inla-grid")
 run_commit_push("explore_aghq")
 run_commit_push("example_naomi")
+
+#' Without experiments
 run_commit_push("dev_sinla")
+
+#' With experiments
+id <- orderly::orderly_run("dev_sinla", param = list(run_experiments = TRUE))
+orderly::orderly_commit(id)
+
 run_commit_push("docs_paper")
 run_commit_push("docs_01-04-20-mini")
 run_commit_push("docs_01-07-21-stats-epi-group")
--- src/dev_sinla/script.R
+++ src/dev_sinla/script.R
@@ -7,20 +7,42 @@ cbpalette <- multi.utils::cbpalette()
 #' Create notebook
 rmarkdown::render("sinla.Rmd")
 
-#' Experiments
-sim_data_m1 <- readRDS("depends/sim_data_m1.rds")
-sim_data_m10 <- readRDS("depends/sim_data_m10.rds")
-sim_data_m100 <- readRDS("depends/sim_data_m100.rds")
-sim_data_m250 <- readRDS("depends/sim_data.rds")
+if(run_experiments) {
+  #' Experiments
+  sim_data_m1 <- readRDS("depends/sim_data_m1.rds")
+  sim_data_m10 <- readRDS("depends/sim_data_m10.rds")
+  sim_data_m100 <- readRDS("depends/sim_data_m100.rds")
+  sim_data_m250 <- readRDS("depends/sim_data.rds")
 
-#' IID random effects
-run_analysis_model1(sim_data_m1, "m1")
-run_analysis_model1(sim_data_m10, "m10")
-run_analysis_model1(sim_data_m100, "m100")
-run_analysis_model1(sim_data_m250, "m250")
+  #' IID random effects
+  run_analysis_model1(sim_data_m1, "m1")
+  run_analysis_model1(sim_data_m10, "m10")
+  run_analysis_model1(sim_data_m100, "m100")
+  run_analysis_model1(sim_data_m250, "m250")
 
-#' ICAR random effects
-run_analysis_model1_icar(sim_data_m1, "m1")
-run_analysis_model1_icar(sim_data_m10, "m10")
-run_analysis_model1_icar(sim_data_m100, "m100")
-run_analysis_model1_icar(sim_data_m250, "m250")
+  #' ICAR random effects
+  run_analysis_model1_icar(sim_data_m1, "m1")
+  run_analysis_model1_icar(sim_data_m10, "m10")
+  run_analysis_model1_icar(sim_data_m100, "m100")
+  run_analysis_model1_icar(sim_data_m250, "m250")
+}
+
+if(!run_experiments) {
+  samples <- c(
+    "model1-samples-m1.rds", "model1-samples-m10.rds",
+    "model1-samples-m100.rds", "model1-samples-m250.rds",
+    "model1-icar-samples-m1.rds", "model1-icar-samples-m10.rds",
+    "model1-icar-samples-m100.rds", "model1-icar-samples-m250.rds"
+  )
+
+  for(file in samples) saveRDS(NULL, file = file)
+
+  plots <- c(
+    "model1-plots-m1.pdf", "model1-plots-m10.pdf",
+    "model1-plots-m100.pdf", "model1-plots-m250.pdf",
+    "model1-icar-plots-m1.pdf", "model1-icar-plots-m10.pdf",
+    "model1-icar-plots-m100.pdf", "model1-icar-plots-m250.pdf"
+  )
+
+  for(file in plots) ggsave(filename = file, plot = NULL)
+}
