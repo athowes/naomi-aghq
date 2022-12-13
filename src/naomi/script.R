88e73c76040d97b3c63fbf8119889d4eb70644ff
athowes <adamhowes@outlook.com>
2022-12-13 13:24:11 +0000

    Work on creating sample_aghq to conform with sample_tmb

--- src/naomi/script.R
+++ src/naomi/script.R
@@ -145,7 +145,8 @@ fit <- val
 #' End expose naomi::fit_tmb
 
 #' Begin expose naomi::sample_tmb
-nsample <- 1000
+#' https://github.com/mrc-ide/naomi/blob/65ac94517b910ac517a45f41e824824e1907a3c4/R/tmb-model.R#L624
+nsample <- 10
 rng_seed <- NULL
 random_only <- TRUE
 verbose <- TRUE
@@ -156,28 +157,31 @@ stopifnot(nsample > 1)
 to_tape <- TMB:::isNullPointer(fit$obj$env$ADFun$ptr)
 if (to_tape) fit$obj$retape(FALSE)
 if (!random_only) {
-  if (verbose)
-    print("Calculating joint precision")
+  if (verbose) print("Calculating joint precision")
   hess <- sdreport_joint_precision(fit$obj, fit$par.fixed)
-  if (verbose)
-    print("Inverting precision for joint covariance")
+  if (verbose) print("Inverting precision for joint covariance")
   cov <- solve(hess)
-  if (verbose)
-    print("Drawing sample")
+  if (verbose) print("Drawing sample")
   smp <- mvtnorm::rmvnorm(nsample, fit$par.full, cov)
 } else {
-  r <- fit$obj$env$random
-  par_f <- fit$par.full[-r]
-  par_r <- fit$par.full[r]
-  hess_r <- fit$obj$env$spHess(fit$par.full, random = TRUE)
-  smp_r <- naomi:::rmvnorm_sparseprec(nsample, par_r, hess_r)
-  smp <- matrix(0, nsample, length(fit$par.full))
-  smp[, r] <- smp_r
-  smp[, -r] <- matrix(par_f, nsample, length(par_f), byrow = TRUE)
-  colnames(smp)[r] <- colnames(smp_r)
-  colnames(smp)[-r] <- names(par_f)
+  r <- fit$obj$env$random #' Indices of the random effects
+  par_f <- fit$par.full[-r] #' Mode of the fixed effects
+  par_r <- fit$par.full[r] #' Mode of the random effects
+  hess_r <- fit$obj$env$spHess(fit$par.full, random = TRUE) #' Hessian of the random effects
+  smp_r <- naomi:::rmvnorm_sparseprec(nsample, par_r, hess_r) #' Sample from the random effects
+  smp <- matrix(0, nsample, length(fit$par.full)) #' Create data structure for sample storage
+  smp[, r] <- smp_r #' Store random effect samples
+  smp[, -r] <- matrix(par_f, nsample, length(par_f), byrow = TRUE) #' For fixed effects store mode
+  colnames(smp)[r] <- colnames(smp_r) #' Random effect names
+  colnames(smp)[-r] <- names(par_f) #' Fixed effect names
 }
 if (verbose) print("Simulating outputs")
+
+nrow(smp) #' Number of rows equal number of samples
+ncol(smp) #' Number of columns equal number of parameters (latent field and hyper)
+sum(lengths(tmb_input$par_init)) #' This is the same as ncol(smp)
+
+#' Given samples, use TMB template to generate output samples
 sim <- apply(smp, 1, fit$obj$report)
 r <- fit$obj$report()
 if (verbose) print("Returning sample")
@@ -226,28 +230,20 @@ length(obj$par)
 #' For example with k = 3 points per dimension then we'll get 3^31 ~= 6E14 points
 #' Or with k = 2 points per dimension 2^31 ~= 2E9
 #' Let's set k = 1 (empirical Bayes) for now, and get all of the infrastructure working first
-
-#' Using the branch https://github.com/athowes/aghq/tree/issue6 for now on this to fix issue with k = 1
-#' Will PR this to master soon (submitted! -- working through)
 start_eb_quad <- Sys.time()
 eb_quad <- aghq::marginal_laplace_tmb(obj, k = 1, startingvalue = obj$par)
 end_eb_quad <- Sys.time()
 time_eb_quad <- end_eb_quad - start_eb_quad
 
-#' The default number of samples for naomi::sample_tmb is 1000, so we will use that as well
-#' Note that with k = 1, the sample marginal algorithm is just returning the mode over and over
-#' Is this a bug or a feature?
-samp <- aghq::sample_marginal(eb_quad, 1000)
-
 n_hyper <- length(obj$par)
 
 #' With k = 2 and ndConstruction = "sparse" we get 63 points -- should be pretty feasible
-sparse_grid <- mvQuad::createNIGrid(n_hyper, "GHe", 2, "sparse")
-mvQuad::size(sparse_grid)$gridpoints
-
-start_sparse_quad <- Sys.time()
-sparse_quad <- aghq::marginal_laplace_tmb(obj, k = 2, startingvalue = obj$par, basegrid = sparse_grid)
-end_sparse_quad <- Sys.time()
+# sparse_grid <- mvQuad::createNIGrid(n_hyper, "GHe", 2, "sparse")
+# mvQuad::size(sparse_grid)$gridpoints
+#
+# start_sparse_quad <- Sys.time()
+# sparse_quad <- aghq::marginal_laplace_tmb(obj, k = 2, startingvalue = obj$par, basegrid = sparse_grid)
+# end_sparse_quad <- Sys.time()
 
 #' TODO: Run aghq::marginal_laplace_tmb line by line here to explain the following error
 # Error in matrix(NA, nrow = No.Points, ncol = dim) :
@@ -270,8 +266,65 @@ str(val)
 str(fit)
 str(fit$sample)
 
-#' This should be quite easy to reproduce in aghq
-str(samp)
-str(samp$samps) #' 633 x 1000 matrix (a.k.a. x samples)
-str(samp$theta) #' 31 x 100 dataframe (a.k.a. theta samples)
+#' Should be quite easy to reproduce in aghq. samp has three elements:
+str(samp$samps) #' 633 x 10 matrix (a.k.a. x samples)
+str(samp$theta) #' 31 x 10 dataframe (a.k.a. theta samples)
 str(samp$thetasamples) #' List of 31
+
+sample_aghq <- function(quad, obj, M, verbose = TRUE) {
+  # Note that with k = 1, sample_marginal just returns the mode for the hypers
+  if (verbose) print("Sampling from aghq")
+  samp <- aghq::sample_marginal(quad, M)
+
+  # This part replaces samples from TMB with samples from aghq
+  if (verbose) print("Rearranging samples")
+  r <- obj$env$random
+  smp <- matrix(0, M, length(obj$env$par))
+  smp[, r] <- unname(t(samp$samps))
+  smp[, -r] <- unname(t(samp$theta))
+  smp <- as.data.frame(smp)
+  colnames(smp)[r] <- rownames(samp$samps)
+  colnames(smp)[-r] <- names(samp$theta)
+
+  # This part is the same as TMB
+  if (verbose) print("Simulating from model")
+  sim <- apply(smp, 1, obj$report)
+  r <- fit$obj$report()
+  if (verbose) print("Returning sample")
+  quad$sample <- Map(vapply, list(sim), "[[", lapply(lengths(r), numeric), names(r))
+  is_vector <- vapply(fit$sample, inherits, logical(1), "numeric")
+  quad$sample[is_vector] <- lapply(quad$sample[is_vector], matrix, nrow = 1)
+  names(quad$sample) <- names(r)
+
+  quad
+}
+
+eb_quad <- sample_aghq(eb_quad, obj, M = 10)
+
+quad <- eb_quad
+M <- 10
+verbose <- TRUE
+
+# Note that with k = 1, sample_marginal just returns the mode for the hypers
+if (verbose) print("Sampling from aghq")
+samp <- aghq::sample_marginal(quad, M)
+
+# This part replaces samples from TMB with samples from aghq
+if (verbose) print("Rearranging samples")
+r <- obj$env$random
+smp <- matrix(0, M, length(obj$env$par))
+smp[, r] <- unname(t(samp$samps))
+smp[, -r] <- unname(t(samp$theta))
+smp <- as.data.frame(smp)
+colnames(smp)[r] <- rownames(samp$samps)
+colnames(smp)[-r] <- names(samp$theta)
+
+# This part is the same as TMB
+if (verbose) print("Simulating from model")
+sim <- apply(smp, 1, obj$report)
+r <- fit$obj$report()
+if (verbose) print("Returning sample")
+quad$sample <- Map(vapply, list(sim), "[[", lapply(lengths(r), numeric), names(r))
+is_vector <- vapply(fit$sample, inherits, logical(1), "numeric")
+quad$sample[is_vector] <- lapply(quad$sample[is_vector], matrix, nrow = 1)
+names(quad$sample) <- names(r)
