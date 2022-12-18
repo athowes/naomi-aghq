9bff9828ffdf29c4fce5b13e2f5c116ff82a25f5
athowes <adamhowes@outlook.com>
2022-12-18 12:12:29 +0000

    Update flowchart

--- src/plot-tikz_algorithm-flowchart/script.R
+++ src/plot-tikz_algorithm-flowchart/script.R
@@ -3,3 +3,6 @@
 # setwd("src/plot-tikz_algorithm-flowchart")
 
 tools::texi2dvi("algorithm-flowchart.tex", pdf = TRUE, clean = TRUE)
+
+system("convert -density 300 -background white -alpha background -alpha off algorithm-flowchart.pdf algorithm-flowchart.png")
+
