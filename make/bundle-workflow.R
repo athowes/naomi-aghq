3667edc898518b3f693f273a866e415fd1b114e5
athowes <adamhowes@outlook.com>
2023-01-11 12:05:39 +0000

    DIDEHPC orderly bundle working for prev-anc-art_sim

--- make/bundle-workflow.R
+++ make/bundle-workflow.R
@@ -1,34 +1,46 @@
 #' A. On local machine:
 #' 1. Create bundle
 #' 2. Upload to sharepoint somewhere with spud
-#' B. On windows VM:
+#'
+#' B. On windows VM, or other drive connected to DIDEHPC:
 #' 1. Pull bundle
 #' 2. Run bundle
 #' 3. Upload to sharepoint
+#'
 #' C. On local machine
 #' 1. Pull bundle
 #' 2. Import into archive
 
+repo <- "elgm-inf"
+report <- "prev-anc-art_sim"
+path_bundles <- "bundles"
+param <- NULL
+
 #' A1.
-path_bundles <- "bundle-input"
-bundle <- orderly::orderly_bundle_pack(path_bundles, "prev-anc-art_model0-N")
+bundle <- orderly::orderly_bundle_pack(path = path_bundles, name = report, parameters = param)
 
 #' A2.
 spud <- spud::sharepoint$new("https://imperiallondon.sharepoint.com/")
-folder <- spud$folder("HIVInferenceGroup-WP", "Shared Documents/orderly/naomi-inf/bundle-input", verify = TRUE)
+folder <- spud$folder("HIVInferenceGroup-WP", paste0("Shared Documents/orderly/", repo, "/", path_bundles), verify = TRUE)
 folder$upload(path = bundle$path)
+recent_bundle <- folder$list() %>% filter(created == max(created))
 
-recent_bundle <- folder$list() %>%
-  filter(created == max(created))
+#' B
+root <- "/Volumes/ath19"
+setwd(root)
+repo_location <- paste0("~/Documents/waterloo/", repo, "/")
 
 #' B1.
 folder$download(
   path = recent_bundle$name,
-  dest = paste0("bundle-input/", recent_bundle$name)
+  dest = file.path(root, path_bundles, recent_bundle$name)
 )
 
 #' B2.
-orderly_packages <- yaml::read_yaml(file.path("src/prev-anc-art_model0-N/orderly.yml"))$packages
+orderly_packages <- yaml::read_yaml(
+  file.path(paste0(repo_location, "src/", report, "/orderly.yml"))
+)$packages
+
 packages <- list(loaded = c("orderly", orderly_packages))
 
 config <- didehpc::didehpc_config(
@@ -39,17 +51,39 @@ config <- didehpc::didehpc_config(
   # "fi--didemrchnb"
 )
 
-src <- conan::conan_sources("awstringer1/aghq")
+# src <- conan::conan_sources(
+#   packages = "github::awstringer1/aghq"
+# )
+
+src <- NULL
 
 ctx <- context::context_save(
   "context",
   packages = packages,
   package_sources = src
-)
+)ß
 
 obj <- didehpc::queue_didehpc(ctx, config = config)
 
-t <- obj$enqueue(orderly::orderly_bundle_run(recent_bundle$name, "bundle-output"))
+#' Test that queue works correctly
+t <- obj$enqueue(sessionInfo())
+t$status()
+t$result()
+
+#' Run larger job
+path <- file.path(recent_bundle$name)
+output_path <- "output"
+
+t <- obj$enqueue(orderly::orderly_bundle_run(
+  path = path,
+  workdir = output_path
+))
 
 t$status()
 t$result()
+
+#' C
+bundle_output_location <- file.path(root, path_bundles, output_path, t$result()$filename)
+orderly::orderly_bundle_import(path = bundle_output_location, root = repo_location)
+
+#' It worked! :party:
