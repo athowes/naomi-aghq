37726effc289540e64b4a921e176a40d2397b008
athowes <adamhowes@outlook.com>
2023-02-01 10:30:02 +0000

    More exploration of the k[j] grid

--- src/naomi-simple_fit/adaptive-k-dev.R
+++ src/naomi-simple_fit/adaptive-k-dev.R
@@ -12,8 +12,39 @@ for(i in seq_along(hypers)) {
   sds[i] <- sd(fit$sample[[hypers[i]]])
 }
 
-#' TODO Turn this into an interval plot with parameter names along one side
-#' Maybe needs a coord_flip()
+#' Interval plot with parameter names along one side
 data.frame(name = hypers, mean = means, sd = sds) %>%
-  ggplot(aes(x = mean, y = sd)) +
-  geom_point()
+  ggplot(aes(x = name, y = mean)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = mean - 2 * sd, ymax = mean + 2 * sd)) +
+  coord_flip() +
+  labs(x = "", y = "") +
+  scale_x_discrete(position = "top") +
+  theme_minimal()
+
+#' Total number of points if it were two grid points per dimension
+formatC(2^length(hypers), format = "e")
+
+#' Total number of points if it were three grid points per dimension
+formatC(3^length(hypers), format = "e")
+
+cut_offs <- c(0, 1, 1.5)
+k_options <- c(1, 2, 3)
+
+ks <- vector(mode = "numeric", length = length(hypers))
+
+ks[sds > cut_offs[1]] <- k_options[1]
+ks[sds > cut_offs[2]] <- k_options[2]
+ks[sds > cut_offs[3]] <- k_options[3]
+
+#' The total number of grid points in a product grid would be
+prod(k)
+
+grids <- list()
+
+for(i in seq_along(hypers)) {
+  grids[[i]] <- mvQuad::createNIGrid(1, "GHe", level = ks[i], "product")
+}
+
+#' Is there a way to take a product of mvQuad grids outside of the function?
+grids[[1]]
