be771f37edd5a06636c422a7116665e9b76cc1a2
athowes <adamhowes@outlook.com>
2023-02-24 15:48:51 +0000

    Work on BayesComp poster

--- src/docs_bayescomp-poster/script.R
+++ src/docs_bayescomp-poster/script.R
@@ -1,5 +1,11 @@
 # orderly::orderly_develop_start("docs_bayescomp-poster")
 # setwd("src/docs_bayescomp-poster")
 
+#' To-do
+#' * [ ] Add MRC GIDA logo
+#' * [ ] Add Oxford logo
+#' * [ ] Add algorithm flowchart
+#' * [ ]
+
 rmarkdown::render("bayescomp-poster.Rmd")
 pagedown::chrome_print("bayescomp-poster.html")
