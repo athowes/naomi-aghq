846a88de0d2fc62a97fdf91c0520c72f21981505
athowes <adamhowes@outlook.com>
2023-03-02 14:48:40 +0000

    Writing out problem for scaling up grid

--- src/naomi-simple_fit/dev/aghq-grid-scale-up.R
+++ src/naomi-simple_fit/dev/aghq-grid-scale-up.R
@@ -1,10 +1,27 @@
 #' Testing how long it takes to fit the aghq for varying grid sizes
 
-#' (This is the TMB fit)
+#' Fit TMB model
+fit <- local_fit_tmb(tmb_inputs_simple, outer_verbose = TRUE, inner_verbose = FALSE, max_iter = 250, progress = NULL, DLL = "naomi_simple")
+
 hypers <- names(fit$par)
 length(hypers)
 
 sd_out <- sdreport(obj = fit$obj, par.fixed = fit$par, getJointPrecision = TRUE)
+sd <- sqrt(diag(sd_out$cov.fixed))
+
+#' Some hypers have high SD than others in part due to their scale
+#' For example if the hyper is on the logit scale then it will have a higher SD
+#' than another hyper on the log scale. Is there a way that I could work around
+#' this? Would like to know how much variation in the particular hyper effects
+#' variation in outputs
+
+data.frame(par = names(sd), sd = unname(sd)) %>%
+  ggplot(aes(x = reorder(par, sd), y = sd)) +
+    geom_point() +
+    coord_flip() +
+    lims(y = c(0, 2)) +
+    theme_minimal() +
+    labs(x = "", y = "SD from TMB")
 
 base_grid <- sd_levels_ghe_grid(
   dim = length(hypers),
