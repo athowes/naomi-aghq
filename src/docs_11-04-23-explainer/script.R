2786e3621356a3f3f4ec9ceeb3eee30da12a5508
athowes <adamhowes@outlook.com>
2023-03-27 00:20:05 +0100

    Add LA example, AGHQ example to slides #25

--- src/docs_11-04-23-explainer/script.R
+++ src/docs_11-04-23-explainer/script.R
@@ -2,6 +2,4 @@
 # orderly::orderly_develop_start("docs_11-04-23-explainer")
 # setwd("src/docs_11-04-23-explainer")
 
-cbpalette <- multi.utils::cbpalette()
-
 rmarkdown::render("11-04-23-explainer.Rmd")
