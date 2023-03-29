58054cdfa0a9fc933d4ca9d18332c356c4af567f
athowes <adamhowes@outlook.com>
2023-03-29 17:58:04 +0100

    Correct date (ICL closed 11th) and edit slides a little post chat with Jeff #24

--- src/docs_11-04-23-explainer/script.R
+++
@@ -1,5 +0,0 @@
-#' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("docs_11-04-23-explainer")
-# setwd("src/docs_11-04-23-explainer")
-
-rmarkdown::render("11-04-23-explainer.Rmd")
