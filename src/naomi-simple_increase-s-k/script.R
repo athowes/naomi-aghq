2796ceec9f10b0e48e4f3d6ddb65711db9983751
athowes <adamhowes@outlook.com>
2023-04-05 15:01:20 +0100

    Add template for notebook about increasing the s and k parameters for simplified Naomi #22

---
+++ src/naomi-simple_increase-s-k/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("naomi-simple_increase-s-k")
+# setwd("src/naomi-simple_increase-s-k")
+
+rmarkdown::render("increase-s-k.Rmd")
