3839c2f4b77351fb1739544fa70d4bb25f83a86b
athowes <adamthowes@gmail.com>
2023-04-20 23:03:40 +0100

    Create placeholder presentation for MLGH network

--- src/docs_18-04-23-explainer/script.R
+++
@@ -1,5 +0,0 @@
-#' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("docs_18-04-23-explainer")
-# setwd("src/docs_18-04-23-explainer")
-
-rmarkdown::render("18-04-23-explainer.Rmd")
---
+++ src/docs_21-06-23-mlgh/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("docs_21-06-23-mlgh")
+# setwd("src/docs_21-06-23-mlgh")
+
+rmarkdown::render("21-06-23-mlgh.Rmd")
