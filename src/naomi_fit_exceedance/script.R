f752d7c4529d0024e6d80c55a94daa2b12d0fcf7
athowes <adamthowes@gmail.com>
2023-04-27 15:11:58 +0100

    Template for #35

---
+++ src/naomi_fit_exceedance/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("naomi-simple_exceedance")
+# setwd("src/naomi-simple_exceedance")
+
+rmarkdown::render("exceedance.Rmd")
