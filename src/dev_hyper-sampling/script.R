40c8c42e5435da4c64c3559d09ac3f4c6f6aef6d
athowes <adamthowes@gmail.com>
2023-05-02 11:52:03 +0100

    Template for notebook on hyper sampling #32

---
+++ src/dev_hyper-sampling/script.R
@@ -0,0 +1,6 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("dev_hyper-sampling")
+# setwd("src/dev_hyper-sampling")
+
+#' Create notebook
+rmarkdown::render("hyper-sampling.Rmd")
