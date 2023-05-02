dca31548613407395a5b59e50ffdca77a83b1f93
athowes <adamthowes@gmail.com>
2023-05-02 16:25:59 +0100

    Move dev/debug-sd-index.R into check_sd-estimation #27

--- src/check_sd-estimation/script.R
+++ src/check_sd-estimation/script.R
@@ -101,7 +101,7 @@ plot +
 #' Does the mismatched j correspond to the AGHQ posterior?
 sd_j1 <- sqrt(cov[j1, j1])
 
-#' Looks possible!
+#' It does looks plausible..
 df %>%
   filter(method == "aghq") %>%
   ggplot(aes(x = samples, fill = method)) +
@@ -117,3 +117,87 @@ length(colnames(cov)) == length(names(aghq$quad$modesandhessians[["mode"]][[1]])
 aghq_hess <- aghq$quad$modesandhessians[["H"]][[1]]
 aghq_cov <- solve(aghq_hess)
 sqrt(aghq_cov[j1, j1])
+
+#' Below is copied over from debug-sd-index.R
+
+samp <- aghq::sample_marginal(aghq$quad, 1000)
+
+j1 <- which(rownames(samp$samps) == par)[i]
+
+(plot1 <- ggplot(data.frame(x = samp$samps[j1, ])) +
+    geom_histogram(aes(x = x, y = after_stat(density)), fill = "#009E73", alpha = 0.7) +
+    labs(x = "", y = "", title = "AGHQ k = 1") +
+    theme_minimal())
+
+aghq_mode <- aghq$quad$modesandhessians[["mode"]][[1]]
+aghq_hess <- aghq$quad$modesandhessians[["H"]][[1]]
+aghq_cov <- solve(aghq_hess)
+
+(plot1 <- plot1 +
+    stat_function(fun = dnorm, args = list(mean = aghq_mode[j1], sd = sqrt(aghq_cov[j1, j1]))))
+
+#' Want to look at sdreport$jointPrecision
+sdreport <- sdreport(tmb$fit$obj, par.fixed = fit$par.fixed, getJointPrecision = TRUE)
+
+(plot2 <- ggplot(data.frame(x = tmb$fit$sample[[par]][i, ])) +
+    geom_histogram(aes(x = x, y = after_stat(density)), fill = "#56B4E9", alpha = 0.7) +
+    labs(x = "", y = "", title = "TMB") +
+    theme_minimal())
+
+tmb_mode <- tmb$fit$par.full
+j2 <- which(names(tmb_mode) == par)[i]
+
+tmb_hess <- naomi:::sdreport_joint_precision(tmb$fit$obj, tmb$fit$par.fixed)
+tmb_cov <- solve(tmb_hess)
+
+(plot2 <- plot2 +
+    stat_function(fun = dnorm, args = list(mean = tmb_mode[j2], sd = sqrt(tmb_cov[j2, j2]))))
+
+#' Observe difference between AGHQ and TMB
+plot1 + plot2
+
+#' Look at the two Hessians
+image(tmb_hess)
+image(aghq_hess)
+
+#' Try excluding the hyperparameters from the TMB Hessian
+hyper_indices <- which(colnames(tmb_hess) %in% names(fit$par))
+tmb_hess_reduced <- tmb_hess[-hyper_indices, -hyper_indices]
+image(tmb_hess_reduced)
+
+tmb_cov_reduced <- solve(tmb_hess_reduced)
+
+#' Consistent with the hypothesis that these are the same matrix up to numerical error?
+plot(tmb_cov_reduced - aghq_cov)
+
+#' AGHQ sampling algorithm: multinomial a node, then MVN from the Hessian of that node (for the latent field)
+#' TMB sampling algorithm: MVN from the Hessian of the joint latent field and hyperparameters
+#' If I MVN sample from tmb_cov_reduced then what's the outcome?
+
+#' There is some correlation in here
+plot(tmb_cov[hyper_indices[1], ])
+
+tmb_samples_reduced <- mvtnorm::rmvnorm(n = nsample, mean = fit$par.full[-hyper_indices], sigma = tmb_cov_reduced, method = "eigen", checkSymmetry = FALSE)
+j3 <- which(colnames(tmb_samples_reduced) == par)[i]
+
+(plot3 <- ggplot(data.frame(x = tmb_samples_reduced[, j3])) +
+    geom_histogram(aes(x = x, y = after_stat(density)), fill = "#E69F00", alpha = 0.7) +
+    labs(x = "", y = "", title = "TMB (reduced Hessian)") +
+    theme_minimal())
+
+(plot3 <- plot3 +
+    stat_function(fun = dnorm, args = list(mean = fit$par.full[-hyper_indices][j3], sd = sqrt(tmb_cov_reduced[j3, j3]))))
+
+plot1 + plot2 + plot3 +
+  labs(caption = "AGHQ with k = 1 matches TMB if you use the reduced Hessian")
+
+#' Checking what naomi:::sdreport_joint_precision is doing (it's the same as sdreport)
+obj <- fit$obj
+par.fixed <- fit$par.fixed
+hessian.fixed <- NULL
+bias.correct <- FALSE
+bias.correct.control <- list(sd = FALSE, split = NULL, nsplit = NULL)
+ignore.parm.uncertainty <- FALSE
+skip.delta.method <- FALSE
+
+# ...
