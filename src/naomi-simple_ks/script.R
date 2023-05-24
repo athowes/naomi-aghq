135202d42de41888f8a118ac0daec149d1d8f616
athowes <adamthowes@gmail.com>
2023-05-24 20:30:49 +0100

    Example KS plot into manuscript

--- src/naomi-simple_ks/script.R
+++ src/naomi-simple_ks/script.R
@@ -4,8 +4,8 @@
 
 rmarkdown::render("ks.Rmd")
 
-beta_rho <- histogram_and_ecdf("beta_rho", i = 1, return_df = TRUE)
-saveRDS(beta_rho$df, "beta_rho.rds")
+beta_alpha <- histogram_and_ecdf("beta_alpha", return_df = TRUE)
+saveRDS(beta_alpha$df, "beta_alpha.rds")
 
 beta_anc_rho <- histogram_and_ecdf("beta_anc_rho", return_df = TRUE)
 saveRDS(beta_anc_rho$df, "beta_anc_rho.rds")
