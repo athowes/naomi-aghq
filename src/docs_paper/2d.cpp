544d86afcf211a4725d832a10abdce87f6fe6adb
athowes <adamthowes@gmail.com>
2023-06-13 19:54:29 +0100

    First shot at an interesting 2D non-Gaussian distribution

--- src/docs_paper/2d.cpp
+++ src/docs_paper/2d.cpp
@@ -8,9 +8,14 @@ Type objective_function<Type>::operator()()
   PARAMETER(theta1);
   PARAMETER(theta2);
 
-  // Initialise negative log-likelihood
   Type nll;
   nll = Type(0.0);
+
+  Type phi1 = 0.2 * theta1 + 0.3 * theta2;
+  Type phi2 = 0.6 * theta1 + 0.1 * theta2;
+
+  nll -= dsn(phi1, Type(2.0), true);
+  nll -= dsn(phi2, Type(-2.0), true);
 
   return(nll);
 }
