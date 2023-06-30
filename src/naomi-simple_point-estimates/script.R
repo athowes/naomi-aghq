52be5080773bbc4fc77377afb0ce51ae5ee634b7
athowes <adamthowes@gmail.com>
2023-06-30 19:36:54 +0100

    Point estimates rescue completed, you're safe now #56

---
+++ src/naomi-simple_point-estimates/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("naomi-simple_point-estimates")
+# setwd("src/naomi-simple_point-estimates")
+
+rmarkdown::render("point-estimates.Rmd")
