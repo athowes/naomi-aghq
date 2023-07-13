ecff46c114fabdce11cd9aad94b7dc6b72615629
athowes <adamthowes@gmail.com>
2023-07-13 16:18:32 +0100

    Create the scaled PCA grid for Naomi (first try) and plot nodes on top of marginals -- may have gotten the better coverage as desired #58

--- src/naomi-simple_fit/dev/pca-scaled.R
+++ src/naomi-simple_fit/dev/pca-scaled.R
@@ -68,6 +68,62 @@ Cs <- diag(1 / sqrt(mean_var)) %*% C %*% diag(1 / sqrt(mean_var))
 eigenCs <- eigen(Cs)
 rownames(eigenCs$vectors) <- hyper_names
 
+lambda_CsC <- apply(eigenCs$vectors, 2, as.numeric(v %*% C %*% v / v %*% v))
+
+eigenC <- eigen(C)
+lambda_CC <- apply(eigenC$vectors, 2, as.numeric(v %*% C %*% v / v %*% v))
+
+data.frame(x = 1:length(lambda_CsC), Cs = cumsum(lambda_CsC) / sum(lambda_CsC), C = cumsum(lambda_CC) / sum(lambda_CC)) %>%
+  pivot_longer(cols = c("Cs", "C"), names_to = "method", values_to = "value") %>%
+  ggplot(aes(x = x, y = value, col = as.factor(method))) +
+  geom_line() +
+  scale_color_brewer(type = "qual") +
+  geom_hline(yintercept = 0.9, col = "grey", linetype = "dashed") +
+  annotate("text", x = 5, y = 0.875, label = "90% of total variation explained", col = "grey") +
+  scale_y_continuous(labels = scales::percent) +
+  labs(
+    title = "The scaled decomposition explains less variance in the covariance matrix",
+    x = "PCA dimensions included", y = "Total variation in C explained", col = "Using the eigendecomposition of"
+  ) +
+  theme_minimal()
+
+s <- 8
+k <- 3
+d <- nrow(C)
+gg_s <- mvQuad::createNIGrid(dim = s, type = "GHe", level = k)
+nodes_out <- t(eigenCs$vectors[, 1:s] %*% diag(sqrt(lambda_CsC[1:s]), ncol = s) %*% t(mvQuad::getNodes(gg_s)))
+for(j in 1:d) nodes_out[, j] <- nodes_out[, j] + m[j] # Adaption
+weights_out <- mvQuad::getWeights(gg_s) * as.numeric(mvQuad::getWeights(mvQuad::createNIGrid(dim = d - s, type = "GHe", level = 1)))
+weights_out <- det(chol(C)) * weights_out # Adaption
+
+gg <- mvQuad::createNIGrid(dim = d, type = "GHe", level = 1)
+gg$level <- rep(NA, times = d)
+gg$ndConstruction <- "PCA"
+gg$nodes <- nodes_out
+gg$weights <- weights_out
+
+#' Import tmbstan from check_hyper-marginals depends
+tmbstan <- readRDS("~/Documents/phd/waterloo/naomi-aghq/src/check_hyper-marginals/depends/tmbstan.rds")
+
+tmbstan_samples <- lapply(
+  1:length(hyper_names),
+  function(i) data.frame("x" = as.numeric(unlist(tmbstan$mcmc$sample[hyper_names[i]])), par = paste0(hyper_names[i]))
+) %>%
+  bind_rows()
+
+scaled_pca_nodes <- gg$nodes %>%
+  as.data.frame() %>%
+  pivot_longer(cols = everything(), names_to = "par", values_to = "node")
+
+ggplot(tmbstan_samples, aes(x = x)) +
+  geom_histogram(fill = "lightgrey", col = "darkgrey", alpha = 0.5) +
+  geom_rug(data = scaled_pca_nodes, aes(x = node), col = "#CC79A7", length = unit(0.1, "npc"), alpha = 0.5) +
+  scale_y_continuous(expand = c(0.2, 0.2)) +
+  facet_wrap(~ par, scales = "free", ncol = 4) +
+  labs(title = "Scaled PCA node positions on hyperparameter marginals from NUTS", x = "", y = "") +
+  theme_minimal() +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
+
 # quad <- local_marginal_laplace_tmb(obj, optresults = optresults, ...)
 # objout <- local_make_tmb_obj(tmb_input$data, tmb_input$par_init, calc_outputs = 1L, inner_verbose, progress, map, DLL = DLL)
 # quad$obj <- objout
