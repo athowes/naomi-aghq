d474d6611c7899a89866803aac4092b9a95eec0d
athowes <adamthowes@gmail.com>
2025-06-09 17:51:50 +0100

    Regenerate with exposed chains and cores

--- utils/pack_rerun.R
+++ utils/pack_rerun.R
@@ -1,7 +1,8 @@
 niter <- 1000
 nthin <- 10
+nchains <- 4
+ncores <- 4
 report <- "naomi-simple_fit"
-param <- list(tmbstan = TRUE, niter = niter, nthin = nthin)
+param <- list(tmbstan = TRUE, niter = niter, nthin = nthin, nchains = nchains, ncores = ncores)
 
 bundle <- orderly::orderly_bundle_pack(path = "naomi-rerun", name = report, parameters = param)
-
