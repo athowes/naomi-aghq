695252748f26bddd72876bfbf3020e46480d8a60
athowes <adamthowes@gmail.com>
2025-07-06 21:47:56 +0100

    Another attempt at good MCMC settings (!)

--- utils/pack_rerun.R
+++ utils/pack_rerun.R
@@ -9,8 +9,8 @@ param <- list(tmbstan = TRUE, niter = niter, nthin = nthin, nchains = nchains, n
 bundle <- orderly::orderly_bundle_pack(path = "naomi-rerun", name = report, parameters = param)
 
 # The real version
-niter <- 120000
-nthin <- 40
+niter <- 80000
+nthin <- 10
 nchains <- 18
 ncores <- 18
 report <- "naomi-simple_fit"
@@ -18,6 +18,12 @@ param <- list(tmbstan = TRUE, niter = niter, nthin = nthin, nchains = nchains, n
 
 bundle <- orderly::orderly_bundle_pack(path = "naomi-rerun", name = report, parameters = param)
 
-recieved_bundle <- "20250619-174833-3548c8b4.zip"
+recieved_bundle <- "20250704-143112-4b78e1d3.zip"
 
 orderly::orderly_bundle_import(path = recieved_bundle, root = NULL)
+
+out <- readRDS("archive/naomi-simple_fit/20250704-143112-4b78e1d3/out.rds")
+mcmc <- out$mcmc
+s <- rstan::summary(mcmc$stanfit)$summary
+s[, "n_eff"] |> summary()
+s[, "Rhat"] |> summary()
